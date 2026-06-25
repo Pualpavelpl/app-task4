19d7ebfecd64d36abd1c1f43b88bb1117ad35dc4
Pavel Plaksin <pplaksin16@gmail.com>
2026-06-26 01:37:48 +0300

    fix stable production cors

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://app-task4.vercel.app"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: true,
+  credentials: true
 }));
 
 
