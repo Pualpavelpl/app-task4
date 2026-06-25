0ac7573a6081640b3ea67f1398f8cec39ccaf637
Pavel Plaksin <pplaksin16@gmail.com>
2026-06-26 00:41:07 +0300

    fix cors for production

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,12 @@ import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
